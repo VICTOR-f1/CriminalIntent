381cce7d1ed7440f7ff3364373d0cd6c1d6a5388
_codefix <ivangorbatov91@gmail.com>
2022-11-16 14:03:04 +0500

    v2 без ошибок 484

--- app/src/main/java/com/example/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/example/criminalintent/CrimeRepository.kt
@@ -4,19 +4,12 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.Room
 import com.example.criminalintent.database.CrimeDatabase
-import com.example.criminalintent.database.migration_1_2
 import java.util.*
 import java.util.concurrent.Executors
 
 private const val DATABASE_NAME = "crime-database"
-
-class CrimeRepository private constructor(context: Context) {
-    private val database: CrimeDatabase = Room.databaseBuilder(
-        context.applicationContext,
-        CrimeDatabase::class.java,
-        DATABASE_NAME
-    ).addMigrations(migration_1_2)
-    .build()
+class CrimeRepository private constructor(context: Context){
+    private val database : CrimeDatabase = Room.databaseBuilder(context.applicationContext, CrimeDatabase::class.java, DATABASE_NAME).build()
     private val crimeDao = database.crimeDao()
     private val executor = Executors.newSingleThreadExecutor()
 
@@ -27,23 +20,22 @@ class CrimeRepository private constructor(context: Context) {
             crimeDao.updateCrime(crime)
         }
     }
-
     fun addCrime(crime: Crime) {
         executor.execute {
             crimeDao.addCrime(crime)
         }
     }
 
-    companion object {
+    companion object{
         private var INSTANCE: CrimeRepository? = null
-        fun initialize(context: Context) {
-            if (INSTANCE == null) {
+        fun initialize(context: Context){
+            if (INSTANCE == null){
                 INSTANCE = CrimeRepository(context)
             }
         }
-
-        fun get(): CrimeRepository {
-            return INSTANCE ?: throw IllegalStateException("CrimeRepository must be initialized")
+        fun get():CrimeRepository{
+            return INSTANCE ?:
+            throw IllegalStateException("CrimeRepository must be initialized")
         }
     }
 }
